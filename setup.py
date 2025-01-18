490242fb158f3d08706aa9d1aac87a9562f7e10b
rahimbaig28 <rahimbaig00332211@gmail.com>
2025-01-18 10:40:47 -0500

    installed all required packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate_speech_classificiation",
+    version="0.1.0",
+    author="rahim baig",
+    author_email="rahimbaig00332211@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
