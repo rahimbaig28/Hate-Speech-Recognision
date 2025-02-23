e038032e240d291cdd2825e0d4a1589066c927a0
rahimbaig28 <rahimbaig00332211@gmail.com>
2025-02-23 10:19:47 -0500

    uodated the train pipeline and supporting files

--- demo.py
+++
@@ -1,7 +0,0 @@
-from hate_speech_detection.logger import logging
-from hate_speech_detection.exception import CustomException
-import sys
-from hate_speech_detection.configuration.gcloud_syncer import GCloudSync
-
-obj = GCloudSync()
-obj.sync_folder_to_gcloud("hate00332211", "data.zip", "data.zip")
